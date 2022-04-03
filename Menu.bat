25cc06afe68db6c725fd2f9258d37f5851a840ad
ArastaD0t <38872983+ArastaD0t@users.noreply.github.com>
2022-04-03 05:10:00 +0200

    Add files via upload

--- Menu.bat
+++ Menu.bat
@@ -1,6 +1,8 @@
 echo off
 title MinecraftAssetsAdder - Menu
 cls
+set WorkingDir=cd
+set mcdir=%appdata%/.minecraft/versions/
 
 :MENU
 echo Select one of this
@@ -17,6 +19,14 @@ IF %M%==3 GOTO EXIT
 :SETUP
 title MinecraftAssetsAdder - Menu - Setup
 echo setup MODE
+SET /P "M1=Specific Minecraft Versions Directory or type DEFAULT:  " 
+IF /I %M1%==DEFAULT (set mcdir=%appdata%/.minecraft/versions) ELSE (set mcdir=%M1%)
+
+echo Default mc version dir is %mcdir%
+
+PAUSE 5
+
+
 GOTO MENU
 :CREATE
 title MinecraftAssetsAdder - Menu - Create
@@ -25,6 +35,12 @@ set /p Name=NameOfRP:
 title MinecraftAssetsAdder - Menu - Create %Name%
 mkdir Output\%Name%
 cd Output\%Name%
+SET /p M2=A.DefaultTextures B.BlankTemplate  
+IF %M2%==A echo Default Textures
+IF %M2%==a echo Default Textures
+IF %M2%==B echo Empty template selected...
+IF %M2%==b echo Empty template selected...
+
 echo Select version
 echo example 1.7 , 1.8, 1.9,.....
 set /p Version=Version: 
@@ -37,12 +53,18 @@ IF %Version%==1.12 SET For=3 & GOTO MAKE
 IF %Version%==1.13 SET For=4 & GOTO MAKE
 IF %Version%==1.14 SET For=4 & GOTO MAKE
 else 
-echo invalid version
+echo invalid or unsupported version
 echo example 1.7 , 1.8, 1.9,.....
 set /p Version=Version: 
 
 :MAKE
 title MinecraftAssetsAdder - Menu - Create - Make %Name%
+
+IF %M2%==A ECHO %mcdir%/%Version%/%Version%.jar %tmp%/%Version%/%Version%.zip & copy %mcdir%/%Version%/%Version%.jar %tmp%/%Version%/%Version%.zip & powershell Expand-Archive -LiteralPath "%tmp%\%Version%\%Version%.zip" -DestinationPath "%tmp%\%Version%" -Force
+IF %M2%==a echo Default Textures
+IF %M2%==B echo Empty template selected...
+IF %M2%==b echo Empty template selected...
+
 set /p Desc=Description:
 echo    "description": ""%Desc%""
 @echo %Name% , %Version% , %Desc% > %Name%.maa
@@ -55,9 +77,11 @@ echo  }
 echo }
 )>pack.mcmeta
 echo pack.mcmeta created
+pause 2
+if 
 CLS
 cd ../..
-copy Datas\pack.png .\Output\%Name%
+copy pack.png .\Output\%Name%
 cd Output\%Name%
 cd
 timeout /t 30
@@ -66,8 +90,11 @@ echo Now its time for Icon
 echo .
 echo do you want edit it
 set /p Icon=(y/n):
-IF %Icon%==y mspaint pack.png & pause & echo edited & GOTO SUBFOLDERS
-IF %Icon%==n GOTO SUBFOLDER
+IF %Icon%==y echo after editing done save and close editor & mspaint pack.png & pause & echo edited & GOTO SUBFOLDERS
+IF %Icon%==Y echo after editing done save and close editor & mspaint pack.png & pause & echo edited & GOTO SUBFOLDERS
+IF %Icon%==n GOTO SUBFOLDERS
+IF %Icon%==N GOTO SUBFOLDERS
+
 else
 GOTO SUBFOLDERS
 ::IF %Icon%==n echo continue & cd .. & copy /Datas/pack.png /%Name%/ & pause
@@ -113,7 +140,7 @@ ECHO WHAT NEXT ?
 PAUSE
 title MinecraftAssetsAdder - Menu - Create - Compile %Name%
 ECHO Compile time
-jar -cMf %Name%-%Version%.zip 
+powershell Compress-Archive . ../%Name%-%Version%.zip
 
 GOTO MENU
 
@@ -131,5 +158,8 @@ CLS
 echo.
 echo THx for using US :D
 ECHO.
+cd %WorkingDir%
+erase *.lang
 SLEEP 5
+
 exit
--- MinecraftAssetsAdder.bat
+++ MinecraftAssetsAdder.bat
@@ -7,8 +7,8 @@ echo.
 echo Vyberte jazyk / Select Language
 echo "  1 CZ         |    2 ENG"
 SET /P Lng=(1/2)
-IF %Lng%==1 echo cz > Datas/cz.lang & start Menu.bat & exit
+IF %Lng%==1 echo cz > cz.lang & start Menu.bat & exit
 else
-IF %Lng%==2 echo eng >Datas/eng.lang & start Menu.bat & exit
+IF %Lng%==2 echo eng > eng.lang & start Menu.bat & exit
 else
 GOTO LANG
